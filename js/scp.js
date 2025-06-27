3aa12a47effec2145432b8a851831a12d13f00ea
thanh0702 <thanhkt2k4@gmail.com>
2025-06-27 21:50:44 +0700

    Update scp.js

--- js/scp.js
+++ js/scp.js
@@ -1,1 +1,14 @@
 
+    const productContainer = document.querySelector('.product-container');
+    const leftBtn = document.querySelector('.left-btn');
+    const rightBtn = document.querySelector('.right-btn');
+
+    leftBtn.addEventListener('click', () => {
+        productContainer.scrollBy({ left: -300, behavior: 'smooth' });
+    });
+
+    rightBtn.addEventListener('click', () => {
+        productContainer.scrollBy({ left: 300, behavior: 'smooth' });
+    });
+
+
